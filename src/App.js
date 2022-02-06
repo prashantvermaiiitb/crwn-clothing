165eafe7d7c0aeddccd8993158f9165336ddfe56
Prashant Verma <prashantvermaiiitb@gmail.com>
2022-02-06 20:43:27 +0530

    sign in component built in.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpComponent from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<HomePage />} />
           <Route path='/shop' element={<ShopPage />} />
+          <Route path='/signin' element={<SignInAndSignUpComponent />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignIn from '../sign-in/sign-in.component';
+const SignInAndSignUpComponent = () => {
+    return (
+        <div className='sign-in-and-sign-up'><SignIn /></div>
+    );
+}
+
+export default SignInAndSignUpComponent;
