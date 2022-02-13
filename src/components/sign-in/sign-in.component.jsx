9d4b4f2c61b5b82d2e0055ce7f68272c9379857e
Prashant Verma <prashantvermaiiitb@gmail.com>
2022-02-13 16:42:38 +0530

    included handling for the signup page

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -41,7 +41,7 @@ class SignIn extends Component {
                         label="password" name="password" required type="password" value={this.state.password} handleChange={this.handleChange} />
                     <div className='buttons'>
                         <CustomButton type="submit"> Sign In </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign In With Google</CustomButton>
+                        <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn> Sign In With Google</CustomButton>
                     </div>
                 </form>
             </div>
